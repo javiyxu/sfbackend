2316232b2886a5f9070b29629a26cbae7982de45
alas <janreytanjutco15@gmail.com>
2026-03-23 14:59:00 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const pizzaController = require('../controllers/pizzaController');
 
+
 router.get('/orders', pizzaController.getAllOrders);
 router.get('/orders/:id', pizzaController.getOrderById);
 router.post('/orders', pizzaController.createOrder);
-router.put('/orders', pizzaController.updateOrder);
-router.delete('/orders', pizzaController.deleteOrder);
+router.put('/orders/:id', pizzaController.updateOrder);     
+router.delete('/orders/:id', pizzaController.deleteOrder);   
 
 module.exports = router;
